10f147047bfd3636a1f1d7254d5df137904b6ca6
Hlb_03 <gfietisov@gmail.com>
2022-11-03 15:30:12 +0200

    add new exception dedicated to already registered user

---
+++ src/main/java/org/luggage_delivery/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,27 @@
+package org.luggage_delivery.exceptions;
+/*
+  User: admin
+  Cur_date: 03.11.2022
+  Cur_time: 15:07
+*/
+
+public class UserAlreadyExistsException extends Exception{
+    public UserAlreadyExistsException() {
+    }
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserAlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
